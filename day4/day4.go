ed3742bbf9a526010d0271159bc14a3f5f5492a9
Tim R. Jensen <timrjensen@gmail.com>
2024-12-20 14:00:32 +0100

    FEAT: 20-12-24 14:00

--- day4/day4.go
+++ day4/day4.go
@@ -8,7 +8,8 @@ import (
 )
 
 const (
-	txt = "XMAS"
+	xmas = "XMAS"
+	mas  = "MAS"
 )
 
 var (
@@ -21,14 +22,14 @@ type grid struct {
 }
 
 func (g grid) left(i int) (bool, int) {
-	if i < 0 || (i-1)/g.n < i/g.n {
+	if i < 0 || i >= len(g.buff) || (i-1)/g.n < i/g.n {
 		return false, i
 	}
 	return true, i - 1
 }
 
 func (g grid) right(i int) (bool, int) {
-	if i < 0 || (i+1)/g.n > i/g.n {
+	if i < 0 || i >= len(g.buff) || (i+1)/g.n > i/g.n {
 		return false, i
 	}
 	return true, i + 1
@@ -64,14 +65,18 @@ func (g grid) downRight(i int) (bool, int) {
 	return g.right(i + g.n)
 }
 
-func (g grid) isValidSequence(i int, op func(int) (bool, int)) bool {
-	if ok, j := op(i); ok {
+func (g grid) isValidSequence(i int, op func(int) (bool, int), txt string) bool {
+	if ok, j := op(i); !ok {
+		return false
+	} else {
+		if txt[table[g.buff[i]-'A']+1] != txt[table[g.buff[j]-'A']] {
+			return false
+		}
 		if txt[table[g.buff[j]-'A']] == txt[len(txt)-1] {
 			return true
 		}
-		return g.isValidSequence(j, op)
+		return g.isValidSequence(j, op, txt)
 	}
-	return false
 }
 
 func parse(path string) *grid {
@@ -97,14 +102,43 @@ func task1(g *grid) int {
 	}
 
 	for i := 0; i < len(g.buff); i++ {
-		if g.buff[i] != txt[0] {
+		if g.buff[i] != xmas[0] {
 			continue
 		}
 
 		for _, op := range ops {
-			if ok := g.isValidSequence(i, op); ok {
+			if ok := g.isValidSequence(i, op, xmas); ok {
 				res++
 			}
+		}
+	}
+
+	return res
+}
+
+func task2(g *grid) int {
+	res := 0
+	ops := []func(int) (bool, int){
+		g.upLeft, g.downRight, g.downLeft, g.upRight,
+	}
+	//TODO: update the table and all is well.
+
+	for i := 0; i < len(g.buff); i++ {
+		if g.buff[i] != mas[1] {
+			continue
+		}
+
+		flag := true
+		for j := 0; j < len(ops); j += 2 {
+			if ok, k := ops[j](i); !ok {
+				continue
+			} else {
+				flag = flag && g.isValidSequence(k, ops[j+1], mas)
+			}
+		}
+
+		if flag {
+			res += 1
 		}
 	}
 
@@ -114,6 +148,7 @@ func task1(g *grid) int {
 func main() {
 	g := parse("input.txt")
 	fmt.Printf("Task %v: %v\n", 1, task1(g))
+	fmt.Printf("Task %v: %v\n", 2, task2(g))
 }
 
 func init() {
